128b206a93070d451325a1451b7655d10bf4c0f2
Pedro09Mera <merap697@gmail.com>
2023-04-18 10:12:49 -0500

    vue practica

--- src/components/CajaDatos.vue
+++ src/components/CajaDatos.vue
@@ -2,24 +2,24 @@
         <div>
         <h3>Ingresar sus datos personales</h3>
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" v-model="nombre">
+        <input type="text" id="nombre" v-model="nombre"><br><br>
     
         <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" v-model="apellido">
+        <input type="text" id="apellido" v-model="apellido"><br><br>
     
-        <label for="edad">Edad:</label>
-        <input type="number" id="edad" v-model="edad">
+        <label for="edad">Edad:</label><br>
+        <input type="number" id="edad" v-model="edad"><br><br>
     
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email">
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" v-model="email"><br><br>
     
         <label for="direccion">Direccion:</label>
-        <input type="text" id="direccion" v-model="direccion">
+        <input type="text" id="direccion" v-model="direccion"><br><br><br>
     
         <label for="fechaNacimiento">Fecha de nacimineto:</label>
-        <input type="date" id="fechaNacimiento" v-model="fechaNacimiento">
+        <input type="date" id="fechaNacimiento" v-model="fechaNacimiento"><br><br>
     
-        <button @click="enviarDatos">MOSTRAR DATOS </button>
+        <button @click="enviarDatos">MOSTRAR DATOS </button><br><br>
         <div v-if="datosEnviados">
         <h3>SUS DATOS SON:</h3>
         <p>Su nombre es: {{ nombre }}</p>
